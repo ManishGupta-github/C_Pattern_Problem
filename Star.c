4a253e93d4fb350a713b0d964b015292e779d94a
manish <manishgupta23956@gmail.com>
2025-08-15 17:05:49 +0530

    updated code

--- Star.c
+++ Star.c
@@ -1,14 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    for(int j=1;j<=4;j++)
-    {
-        for(int i=1;i<=5;i++)
-    {
-        printf("*");
-    }
-    printf("\n");
-    }
+    //*****
+    //*****
+    //*****
+    //*****
+    // for(int j=1;j<=4;j++)
+    // {
+    //     for(int i=1;i<=5;i++)
+    // {
+    //     printf("*");
+    // }
+    // printf("\n");
+    // }
 
-return 0;
+    return 0;
 }
