225a8ac64903f6cdbd42413c5853a27631b89c74
manish <manishgupta23956@gmail.com>
2025-08-15 16:46:08 +0530

    commit02

--- Star.c
+++ Star.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello");
-    return 0;
+    for(int j=1;j<=4;j++)
+    {
+        for(int i=1;i<=5;i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    }
+
+return 0;
 }
